d081a9c587aaf0304e843633f7506036f84f6068
Sam Baynham <sam.baynham@wrenkitchens.com>
2025-08-15 16:22:12 +0100

    Tidying up the rollup

--- rollup.config.js
+++ rollup.config.js
@@ -6,27 +6,36 @@ import typescript from '@rollup/plugin-typescript';
 import sass from 'rollup-plugin-sass';
 
 const copyConfig = {
-
   targets: [
-  	{ src: 'resources/images', dest: 'public' },
-        { src: 'resources/fonts', dest: 'public' },
+  	{
+        src: 'resources/images',
+        dest: 'public'
+    },
+    {
+        src: 'resources/fonts',
+        dest: 'public'
+    },
   ]
 };
 
-
-// The main JavaScript bundle for modern browsers that support
-
-// JavaScript modules and other ES2015+ features.
+const terserOptions = {
+    ecma: 2021,
+    module: true,
+    warnings: true,
+    mangle: {
+        properties: {
+            regex: /^__/,
+        },
+    },
+}
 
 const config = {
 
   input: 'resources/typescript/app.ts',
 
   output: {
-
     dir: './public/js/',
     format: 'es',
-
   },
 
   plugins: [
@@ -37,30 +46,15 @@ const config = {
     	failOnError: true,
     }),
     resolve(),
-    terser({
-	    ecma: 2021,
-            module: true,
-            warnings: true,
-            mangle: {
-        	    properties: {
-                        regex: /^__/,
-                    },
-                },
-            }),
-            summary(),
-
+    summary(),
   ],
-
   preserveEntrySignatures: false,
-
 };
 
 
 if (process.env.NODE_ENV !== 'development') {
 
-  config.plugins.push(terser());
+  config.plugins.push(terser(terserOptions));
 
 }
-
-
 export default config;
