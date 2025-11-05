66f824da2798a682fe46300e5c4cd9dc17e29bf2
Sam Baynham <sam.baynham@wrenkitchens.com>
2025-11-05 16:37:28 +0000

    Fixing revocation voter

--- src/Security/InvitationOwnerVoter.php
+++ src/Security/InvitationOwnerVoter.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Security;
 
-use App\Entity\Game;
-use App\Entity\GameInvitation;
+use App\Services\Game\Domain\GameInvitation;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
