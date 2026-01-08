598b4b7c649ad1a87291dedeb70a316a4ee59840
Sam Baynham <sam.baynham@wrenkitchens.com>
2026-01-08 13:42:26 +0000

    Reworked scroll and header detection

--- resources/typescript/components/Header.ts
+++ resources/typescript/components/Header.ts
@@ -1,15 +1,42 @@
 export class Header extends HTMLElement {
+
+    private observer : IntersectionObserver;
+
+    private bodyElement: HTMLBodyElement;
+
+    private logoWrapper: HTMLElement;
+
     constructor() {
         super();
+        const options = {
+            rootMargin: "0px",
+            scrollMargin: "0px",
+            threshold: 0.0,
+        }
+
+        let body: HTMLBodyElement|null = document.querySelector('body');
+        let logoWrapper: HTMLElement|null = document.getElementById('logo-wrapper');
+
+        if (body === null || logoWrapper === null) {
+            throw new Error('Header requires a body and logo wrapper');
+        }
+        this.bodyElement = body;
+        this.logoWrapper = logoWrapper;
+
+        this.observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    this.bodyElement.classList.remove('scrolled');
+                } else {
+                    this.bodyElement.classList.add('scrolled');
+                }
+            })
+        }, options);
+
+        this.observer.observe(this.logoWrapper);
     }
 
     public connectedCallback(): void {
-        document.addEventListener('scroll', () => {
-            if (document.body.scrollTop > this.offsetTop || document.documentElement.scrollTop > (this.offsetTop / 2)) {
-                document.body.classList.add('scrolled');
-            } else {
-                document.body.classList.remove('scrolled');
-            }
-        });
+
     }
 }
