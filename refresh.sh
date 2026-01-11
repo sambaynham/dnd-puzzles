edf0362d21687f046183e427f51aaa872d26bb86
Sam Baynham <sam.baynham@gmail.com>
2026-01-11 01:10:17 +0000

    Adding refresh script

---
+++ refresh.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+
+for i in "$@"; do
+  case $i in
+    -e=*|--mode=*)
+      MODE="${i#*=}"
+      shift # past argument=value
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [[ "$MODE" == "prod" ]]
+then
+    echo "Running in production"
+
+    echo "Exporting Schema"
+    ./bin/console api:openapi:export --yaml --output=swagger_docs.yaml
+
+    echo "Clearing Cache"
+    ./bin/console cache:clear
+
+    echo "Rebuilding Authoritative container"
+    composer dump-autoload
+
+    echo "Running Build"
+    npm run build
+
+    echo "All done. Now restart PHP"
+
+
+else
+  echo "Running in development"
+  echo "Clearing Cache"
+  docker compose --file=../dnd-puzzles-environment/docker-compose.yml exec dnd-puzzles-app bin/console cache:clear
+  echo "Exporting Schema"
+  docker compose --file=../dnd-puzzles-environment/docker-compose.yml exec dnd-puzzles-app bin/console api:openapi:export --yaml --output=swagger_docs.yaml
+fi
+
+echo "Syncing Schema"
+npx openapi-typescript ./swagger_docs.yaml -o ./resources/typescript/schema/schema.d.ts
